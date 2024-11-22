ae70523ce5010e269b407de85e462957ecf89cd7
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-11-22 10:30:59 +0545

    stringrelationaloperator

--- index.js
+++ index.js
@@ -10,9 +10,14 @@
 //a = +a + 2;
 //console.log(a, typeof a);
 
-let num = 1;
-num = num + 2;
-console.log(num, typeof num);
+// let num = 1;
+// num = num + 2;
+// console.log(num, typeof num);
 
-let data = 6 >= 6;
-console.log(data);
+// let data = 6 >= 6;
+// console.log(data);
+
+let data2 = "ange";
+let data3 = "apple";
+let data4 = data2 > data3;
+console.log(data4);
