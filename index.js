4073f366d693c63a59e599c690cc11fa0dfb53ee
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-11-22 10:23:40 +0545

    changes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // console.log("Hello world!");
 
-let a;
-console.log(a, typeof a);
-a = 8;
-a = a - "";
-console.log(a, typeof a);
+//let a;
+//console.log(a, typeof a);
+//a = 8;
+//a = a - "";
+//console.log(a, typeof a);
 // a = a + " ";
 // console.log(a, typeof a);
-a = +a + 2;
-console.log(a, typeof a);
+//a = +a + 2;
+//console.log(a, typeof a);
+
+let num = 1;
+num = num + 2;
+console.log(num, typeof num);
+
+let data = 6 >= 6;
+console.log(data);
