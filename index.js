a71eb44d7c65cfc1b8fb771028b42735b5c39559
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-11-22 08:40:31 +0545

    learnjs

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log("Hello world!");
+
+let a;
+console.log(a, typeof a);
+a = 8;
+a = a - "";
+console.log(a, typeof a);
+// a = a + " ";
+// console.log(a, typeof a);
+a = +a + 2;
+console.log(a, typeof a);
