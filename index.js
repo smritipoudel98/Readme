b083187cbbe13bdffe5247eef6509ed869238328
Smritipoudel190331 <108278547+Smritipoudel190331@users.noreply.github.com>
2024-11-26 08:49:10 +0545

    logical operator

--- index.js
+++ index.js
@@ -18,6 +18,12 @@
 // console.log(data);
 
 let data2 = "ange";
-let data3 = "apple";
+let data3 = 9;
 let data4 = data2 > data3;
 console.log(data4);
+
+let x = 4,
+  y = 5,
+  z = 6;
+let res = x > y || x < z;
+console.log(res);
